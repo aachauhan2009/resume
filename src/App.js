a13e29f4055a0a5fdece7328cba0e73b2b9206ba
Amit Chauhan <a.a.chauhan2009@gmail.com>
2019-09-06 12:03:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ResumeContainer">
+      <div className="Resume">
+        <div className="Side">
+          <div className="Title">
+            <div> Amit Chauhan </div>
+            <div className="SubTitle"> Front-end Developer </div>
+          </div>
+        </div>
+
+        <div className="Main">
+          <div className="Heading"> Profile </div>
+          <div className="About">
+            Innovative, dynamic and quick learner Front-end Developer with over 6 years of experience in building scalable, usable and well-constructed websites and applications.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
